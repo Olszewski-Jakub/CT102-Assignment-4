db90deae0034c17427d73f6fed21bd091b495882
Jakub Olszewski <j.olszewski05@gmail.com>
2024-03-12 22:57:26 +0000

    Removed file_ar - it was tha same as file1. Modiied the value from 4 to 5 to allow another soritng algorithm

--- main.c
+++ main.c
@@ -5,13 +5,11 @@
 
 #define file1 "C://Users//jolsz//uni//freshman//CT102 - Algorithms & Information Systems//assignmnets//Assignment_4//Sorting_Algorithms_Performance_Analysis//files//file1.txt"
 #define file2 "C://Users//jolsz//uni//freshman//CT102 - Algorithms & Information Systems//assignmnets//Assignment_4//Sorting_Algorithms_Performance_Analysis//files//file2.txt"
-#define file_a4 "C://Users//jolsz//uni//freshman//CT102 - Algorithms & Information Systems//assignmnets//Assignment_4//Sorting_Algorithms_Performance_Analysis//files//file_a4.txt"
 
 #define result_file_1 "C://Users//jolsz//uni//freshman//CT102 - Algorithms & Information Systems//assignmnets//Assignment_4//Sorting_Algorithms_Performance_Analysis//files//result_file1.csv"
 #define result_file_2 "C://Users//jolsz//uni//freshman//CT102 - Algorithms & Information Systems//assignmnets//Assignment_4//Sorting_Algorithms_Performance_Analysis//files//result_file2.csv"
-#define result_file_a4 "C://Users//jolsz//uni//freshman//CT102 - Algorithms & Information Systems//assignmnets//Assignment_4//Sorting_Algorithms_Performance_Analysis//files//result_file_a4.csv"
 
-#define NUM_SORTING_ALGORITHMS 4
+#define NUM_SORTING_ALGORITHMS 5
 #define NUM_RESULTS_PER_ALGORITHM 10
 
 
@@ -29,7 +27,6 @@ int main() {
 
     processFile(file1, result_file_1);
     processFile(file2, result_file_2);
-//    processFile(file_a4, result_file_a4);
     return 0;
 }
 
@@ -38,10 +35,10 @@ void processFile(const char *filePath, const char *resultFilePath) {
     SortingResult sortingResultsFile[NUM_SORTING_ALGORITHMS * NUM_RESULTS_PER_ALGORITHM];
 
     // Pass the address of arrIndex and sortingResultsFile to the function
-//    executeSort(numbers, bubbleSort, "Bubble Sort", &arrIndex, sortingResultsFile, filePath);
-//    executeSort(numbers, selectionSort, "Selection Sort", &arrIndex, sortingResultsFile, filePath);
-//    executeSort(numbers, insertionSort, "Insertion Sort", &arrIndex, sortingResultsFile, filePath);
-//    executeSort(numbers, countingSort, "Count Sort", &arrIndex, sortingResultsFile, filePath);
+    executeSort(numbers, bubbleSort, "Bubble Sort", &arrIndex, sortingResultsFile, filePath);
+    executeSort(numbers, selectionSort, "Selection Sort", &arrIndex, sortingResultsFile, filePath);
+    executeSort(numbers, insertionSort, "Insertion Sort", &arrIndex, sortingResultsFile, filePath);
+    executeSort(numbers, countingSort, "Count Sort", &arrIndex, sortingResultsFile, filePath);
     executeSort(numbers, mergeSort, "Merge Sort", &arrIndex, sortingResultsFile, filePath);
 
     printResults(&arrIndex, sortingResultsFile);
