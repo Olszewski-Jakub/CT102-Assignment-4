c279226a3be645fe25460e43599de01fb13005cb
Jakub Olszewski <j.olszewski05@gmail.com>
2024-03-13 00:03:36 +0000

    Added docs for merge sor functions

--- include/SortingAlgorithms.h
+++ include/SortingAlgorithms.h
@@ -6,7 +6,6 @@
 #include "Swap.h"
 
 
-
 /**
  * Sorts an array of integers using the bubble sort algorithm.
  *
@@ -51,8 +50,39 @@ void insertionSort(int nums[], int size, int *swaps, int *comparisons, int *func
  */
 void countingSort(int nums[], int size, int *swaps, int *comparisons, int *functionCalls);
 
+/**
+ * Sorts an array of integers using the merge sort algorithm.
+ *
+ * @param nums The array to be sorted.
+ * @param size The size of the array.
+ * @param swaps A pointer to an integer where the number of swaps will be stored.
+ * @param comparisons A pointer to an integer where the number of comparisons will be stored.
+ * @param functionCalls A pointer to an integer where the number of function calls will be stored.
+ */
 void mergeSort(int nums[], int size, int *swaps, int *comparisons, int *functionCalls);
+
+/**
+ * Recursively sorts an array of integers using the merge sort algorithm.
+ *
+ * @param nums The array to be sorted.
+ * @param lb The lower bound of the section of the array to be sorted.
+ * @param ub The upper bound of the section of the array to be sorted.
+ * @param swaps A pointer to an integer where the number of swaps will be stored.
+ * @param comparisons A pointer to an integer where the number of comparisons will be stored.
+ * @param functionCalls A pointer to an integer where the number of function calls will be stored.
+ */
 void mergeSortRecursive(int nums[], int lb, int ub, int *swaps, int *comparisons, int *functionCalls);
+
+/**
+ * Merges two sorted sections of an array into a single sorted section.
+ *
+ * @param arrA The array containing the sections to be merged.
+ * @param lb The lower bound of the first section to be merged.
+ * @param mid The upper bound of the first section and lower bound of the second section to be merged.
+ * @param ub The upper bound of the second section to be merged.
+ * @param swaps A pointer to an integer where the number of swaps will be stored.
+ * @param comparisons A pointer to an integer where the number of comparisons will be stored.
+ */
 void merge(int arrA[], int lb, int mid, int ub, int *swaps, int *comparisons, int *functionCalls);
 
 #endif //QUESTION3_SORTINGALGORITHMS_H
