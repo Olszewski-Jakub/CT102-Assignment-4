2bf110ca1a92919ce11e351f8d86e8b954983d10
Jakub Olszewski <j.olszewski05@gmail.com>
2024-03-26 16:08:02 +0000

    Added .gitignore

--- cmake-build-debug/CMakeFiles/question3.dir/src/DataProcessing.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/question3.dir/src/DataProcessing.c.obj: \
- C:\Users\jolsz\uni\freshman\CT102\ -\ Algorithms\ &\ Information\ Systems\assignmnets\Assignment_4\Sorting_Algorithms_Performance_Analysis\src\DataProcessing.c \
- C:/Users/jolsz/uni/freshman/CT102\ -\ Algorithms\ &\ Information\ Systems/assignmnets/Assignment_4/Sorting_Algorithms_Performance_Analysis/include/DataProcessing.h
--- cmake-build-debug/CMakeFiles/question3.dir/src/Swap.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/question3.dir/src/Swap.c.obj: \
- C:\Users\jolsz\uni\freshman\CT102\ -\ Algorithms\ &\ Information\ Systems\assignmnets\Assignment_4\Sorting_Algorithms_Performance_Analysis\src\Swap.c \
- C:/Users/jolsz/uni/freshman/CT102\ -\ Algorithms\ &\ Information\ Systems/assignmnets/Assignment_4/Sorting_Algorithms_Performance_Analysis/include/Swap.h
